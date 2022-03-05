079c02bdba80baeb50b5d9bf5a1a1bc5f11132d0
BucBharat <58068566+BucBharat@users.noreply.github.com>
2022-03-05 15:55:55 +0530

    Added NavBar in App.js

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import { AppBar, Toolbar } from '@material-ui/core';
+
+const NavBar = () => {
+  return (
+    <AppBar>
+      <Toolbar></Toolbar>
+    </AppBar>
+  );
+};
+
+export default NavBar;
